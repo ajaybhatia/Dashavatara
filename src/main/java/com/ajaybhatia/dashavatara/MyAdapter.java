8de1772e844c86949ebd8a06d7e61222ce964319
Ajay Bhatia <prof.ajaybhatia@gmail.com>
2015-01-13 20:03:38 +0530

    Created new Adapter with name MyAdapter

--- src/main/java/com/ajaybhatia/dashavatara/MyAdapter.java
+++ src/main/java/com/ajaybhatia/dashavatara/MyAdapter.java
@@ -1,7 +1,27 @@
 package com.ajaybhatia.dashavatara;
 
-/**
- * Created by ajay on 13/1/15.
- */
-public class MyAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class MyAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
 }
