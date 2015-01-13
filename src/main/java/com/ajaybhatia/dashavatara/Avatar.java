5ea78b17bbc5383a6f96f9e7d16644ba9903154b
Ajay Bhatia <prof.ajaybhatia@gmail.com>
2015-01-13 21:39:29 +0530

    Added getters and setters in Avatar.java

--- src/main/java/com/ajaybhatia/dashavatara/Avatar.java
+++ src/main/java/com/ajaybhatia/dashavatara/Avatar.java
@@ -10,4 +10,28 @@ public class Avatar {
         this.title = title;
         this.description = description;
     }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
