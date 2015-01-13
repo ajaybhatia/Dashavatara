b8be402c1477140e9a7a257aa4c91221e1dab0a1
Ajay Bhatia <prof.ajaybhatia@gmail.com>
2015-01-13 21:58:33 +0530

    Fixed a derp

--- src/main/java/com/ajaybhatia/dashavatara/MainActivity.java
+++ src/main/java/com/ajaybhatia/dashavatara/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreat   e(savedInstanceState);
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         ListView listView = getListView();
