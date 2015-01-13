d24a95198a0e2f875f5dc544a5b241f249bbd66b
Ajay Bhatia <prof.ajaybhatia@gmail.com>
2015-01-13 21:42:34 +0530

    Added main logic in MainActivity

--- src/main/java/com/ajaybhatia/dashavatara/MainActivity.java
+++ src/main/java/com/ajaybhatia/dashavatara/MainActivity.java
@@ -2,6 +2,10 @@ package com.ajaybhatia.dashavatara;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
 
 public class MainActivity extends ListActivity {
@@ -10,6 +14,8 @@ public class MainActivity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = getListView();
     }
 
 }
