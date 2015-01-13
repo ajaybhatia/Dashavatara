21ce1c144eedcf05cbcfbe0df55fee633af83ed6
Ajay Bhatia <prof.ajaybhatia@gmail.com>
2015-01-13 19:50:37 +0530

    Removed header from Avatar.java

--- src/main/java/com/ajaybhatia/dashavatara/Avatar.java
+++ src/main/java/com/ajaybhatia/dashavatara/Avatar.java
@@ -1,7 +1,13 @@
 package com.ajaybhatia.dashavatara;
 
-/**
- * Created by ajay on 13/1/15.
- */
 public class Avatar {
+    private int image;
+    private String title;
+    private String description;
+
+    public Avatar(int image, String title, String description) {
+        this.image = image;
+        this.title = title;
+        this.description = description;
+    }
 }
