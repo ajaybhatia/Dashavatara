42d5d28d00226ed3d2d1e72b308c7585012c4d11
Ajay Bhatia <prof.ajaybhatia@gmail.com>
2015-01-16 21:26:22 +0530

    Change MainActivity's base class to ActionBarActivity ans renamed id for ListView in activity_main.xml

--- src/main/java/com/ajaybhatia/dashavatara/MainActivity.java
+++ src/main/java/com/ajaybhatia/dashavatara/MainActivity.java
@@ -18,21 +18,21 @@
 
 package com.ajaybhatia.dashavatara;
 
-import android.app.ListActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.widget.ListView;
 
 import com.ajaybhatia.dashavatara.helpers.MyAdapter;
 
 
-public class MainActivity extends ListActivity {
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView listView = getListView();
+        ListView listView = (ListView) findViewById(R.id.list);
 
         MyAdapter adapter = new MyAdapter(this);
         listView.setAdapter(adapter);
